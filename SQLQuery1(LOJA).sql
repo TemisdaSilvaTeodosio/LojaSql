72348e031ced697285bfd0189a2517d27a86e74f
TemisdaSilvaTeodosio <122544041+TemisdaSilvaTeodosio@users.noreply.github.com>
2024-10-24 11:24:04 -0300

    SQLQUERY1(LOJA).sql

--- SQLQuery1(LOJA).sql
+++ SQLQuery1(LOJA).sql
@@ -1,8 +1,7 @@
-
--- TABLES
+/*TABLES*/
 
 CREATE TABLE Pessoa(
-	idPessoa int,
+	idPessoa int NOT NULL,
 	nome varchar(255) not null,
 	logradouro varchar(255),
 	cidade varchar(255),
@@ -20,32 +19,44 @@ MAXVALUE 1000000
 NO CYCLE 
 CACHE 5;
 
+
 CREATE TABLE Pessoa_Fisica(
-	idPessoa_Fisica int PRIMARY KEY,
+	idPessoa_Fisica INT PRIMARY KEY,
     CPF varchar(11) NOT NULL,
 	idPessoa int,
-    FOREIGN KEY (idPessoa) REFERENCES Pessoa(idPessoa)
-);
+    FOREIGN KEY (idPessoa) REFERENCES Pessoa(idPessoa) 
+); 
+
+
+CREATE INDEX PessoaFisica_FKIndex ON Pessoa_Fisica(idPessoa); 
+
+CREATE INDEX IFK_PessoaFisica ON Pessoa_Fisica (idPessoa);  
+
 
 CREATE TABLE Pessoa_Juridica(
-    idPessoa_Juridica int PRIMARY KEY,
+    idPessoa_Juridica INT PRIMARY KEY,
     CNPJ varchar(14) NOT NULL,
 	idPessoa int,
     FOREIGN KEY (idPessoa) REFERENCES Pessoa(idPessoa)
-);
+); 
+
+CREATE INDEX PessoaJuridica_FKIndex ON Pessoa_Juridica (idPessoa); 
+
+CREATE INDEX IFK_PessoaJuridica ON Pessoa_Juridica (idPessoa); 
+
 
 CREATE TABLE Produto (
-    idProduto INT PRIMARY KEY,
+    idProduto INT IDENTITY PRIMARY KEY ,
     nome VARCHAR(255),
     quantidade INT,
     precoVenda NUMERIC(10, 2)
-);
+); 
 
 CREATE TABLE Usuario(
-    idUsuario INT PRIMARY KEY,
-    Logar VARCHAR(255),
-    Senha VARCHAR(25)
-);
+    idUsuario INT IDENTITY PRIMARY KEY ,
+    Logar VARCHAR(255) not null,
+    Senha VARCHAR(25) not null
+); 
 
 
 CREATE TABLE Movimento(
@@ -59,61 +70,57 @@ CREATE TABLE Movimento(
     FOREIGN KEY (idUsuario) REFERENCES Usuario(idUsuario),
     FOREIGN KEY (idPessoa) REFERENCES Pessoa(idPessoa),
     FOREIGN KEY (idProduto) REFERENCES Produto(idProduto)
-);
+); 
+
+CREATE INDEX Movimento_FKIndex1 ON Movimento (idUsuario); 
+
+CREATE INDEX Movimento_FKIndex2 ON Movimento (idPessoa); 
+
+CREATE INDEX Movimento_FKIndex3 ON movimento (idProduto); 
+
+CREATE INDEX IFK_Responsavel ON Movimento (idUsuario); 
+
+CREATE INDEX IFK_Cliente ON Movimento (idPessoa); 
+
+CREATE INDEX IFK_ItemMovimentado ON Movimento (idProduto); 
 
 
+/*INSERT INTO*/
 
---iNSERT INTO
-
-INSERT INTO Usuario VALUES 
-(1, 'op1', 'op1'), 
-(2, 'op2', 'op2');
-
-INSERT INTO Usuario VALUES
-(3, 'op3', 'op3'),
-(4, 'op4', 'op4');
+INSERT INTO Usuario (Logar, Senha) VALUES ( 'op1', 'op1'); 
+INSERT INTO Usuario (Logar, Senha)VALUES ( 'op2', 'op2');
+INSERT INTO Usuario (Logar, Senha)VALUES ( 'op3', 'op3');
+INSERT INTO Usuario (Logar, Senha)VALUES ( 'op4', 'op4');
 
 INSERT INTO Produto VALUES 
-(1, 'Banana', 100, 5.00),
-(2, 'Maçã', 300, 3.50),
-(3, 'Laranja', 500, 2.00),
-(4, 'Manga', 800, 4.00);
+( 'Banana', 100, 5.00),
+('MaÃ§Ã£', 300, 3.50),
+('Laranja', 500, 2.00),
+('Manga', 800, 4.00);
 
-select * from Usuario;
-Select * from Produto;
+SELECT * FROM Usuario;
 
-
-
-INSERT INTO Movimento(idPessoa, idUsuario, idProduto, quantidade, tipo, valorUnitario)
-	VALUES	(8,1,1,12,'S',4.00),
-			(9,2,2,2,'S',3.00),
-			(10,3,3,35,'E',4.00),
-			(11,4,4,25,'E',6.00);
-
-
-
-
-INSERT INTO Pessoa
-(idPessoa, nome, logradouro,cidade, estado, telefone, email)
-values 
-(next value for seq_Pessoa,'Ferdinando','Rua 21, casa 411, Japiim', 'Manaus', 'AM', '921313-1313','Toro@Ferdinando.com'),
-(next value for seq_Pessoa, 'JJC', 'Rua 11, Centro', 'Riacho do Norte', 'PA', '1212-1212', 'jjc@riacho.com');  
-
-INSERT INTO Pessoa
-(idPessoa, nome, logradouro,cidade, estado, telefone, email)
-values
-(next value for seq_Pessoa, 'Marta','Rua 31 de março, 504', 'Manaus', 'AM', '1212-2525', 'Marta@toledo.com'),
-(next value for seq_Pessoa, 'Smart', 'Laranjeiras, 0000', 'Manaus', 'AM','4585-9674', 'Smart@CIA.com')
-;
 
 
 DECLARE @IdPessoa int;
-SET @IdPessoa = Next value for seq_Pessoa; 
+SET @IdPessoa = Next value for seq_Pessoa;
 
-INSERT INTO Pessoa_Fisica
-(idPessoa_Fisica, CPF)
+
+INSERT INTO Pessoa
+(idPessoa, nome, logradouro,cidade, estado, telefone, email)
 VALUES 
+(@IdPessoa, 'Ferdinando','Rua 21, casa 411, Japiim', 'Manaus', 'AM', '921313-1313','Toro@Ferdinando.com');
+
+INSERT INTO Pessoa_Fisica 
+(idPessoa_Fisica, CPF)
+VALUES
 (@IdPessoa, '78945612378');
+
+
+INSERT INTO Pessoa
+(idPessoa, nome, logradouro,cidade, estado, telefone, email)
+VALUES 
+(@IdPessoa, 'JJC', 'Rua 11, Centro', 'Riacho do Norte', 'PA', '1212-1212', 'jjc@riacho.com');
 
 INSERT INTO Pessoa_Juridica
 (idPessoa_Juridica, CNPJ)
@@ -121,7 +128,45 @@ VALUES
 (@IdPessoa, '15915915945682');
 
 
---DADOS
+
+INSERT INTO Pessoa
+(idPessoa, nome, logradouro,cidade, estado, telefone, email)
+VALUES 
+(@IdPessoa, 'Marta','Rua 31 de marÃ§o, 504', 'Manaus', 'AM', '1212-2525', 'Marta@toledo.com');
+
+INSERT INTO Pessoa_Fisica 
+(idPessoa_Fisica, CPF)
+VALUES
+(@IdPessoa, '15935862475');
+
+
+
+
+INSERT INTO Pessoa
+(idPessoa, nome, logradouro,cidade, estado, telefone, email)
+VALUES
+(@IdPessoa, 'Smart', 'Laranjeiras, 0000', 'Manaus', 'AM','4585-9674', 'Smart@CIA.com');
+
+INSERT INTO Pessoa_Juridica
+(idPessoa_Juridica, CNPJ)
+VALUES
+(@IdPessoa, '4569871236985');
+
+
+
+
+
+
+INSERT INTO Movimento(idPessoa, idUsuario, idProduto, quantidade, tipo, valorUnitario)
+	VALUES	(1,1,1,12,'S',4.00),
+			(2,2,2,2,'S',3.00),
+			(4,3,3,35,'E',4.00),
+			(5,4,4,25,'E',6.00);
+
+SELECT * FROM Movimento;
+
+
+/*DADOS*/
 
 SELECT * FROM Usuario;
 SELECT * FROM Produto;
@@ -130,16 +175,76 @@ SELECT * FROM Pessoa_Fisica;
 SELECT * FROM Pessoa_Juridica;
 SELECT * FROM Movimento;
 
+SELECT * FROM Pessoa 
+inner join Pessoa_Fisica ON (Pessoa.idPessoa = Pessoa_Fisica.idPessoa);
+
+SELECT * FROM Pessoa 
+inner join Pessoa_Juridica ON (Pessoa.idPessoa = Pessoa_Juridica.idPessoa);
+
+SELECT 
+	p.nome, 
+	pe.nome, 
+	m.quantidade, 
+	m.valorUnitario, 
+	m.quantidade * m.valorUnitario AS ValorTotal 
+FROM Movimento AS m
+inner join Produto AS p ON (m.idProduto = p.idProduto)
+inner join Pessoa AS pe ON (m.idPessoa = pe.idPessoa)
+WHERE tipo = 'S'
+
+SELECT 
+	p.nome, 
+	pe.nome, 
+	m.quantidade, 
+	m.valorUnitario, 
+	m.quantidade * m.valorUnitario AS ValorTotal 
+FROM Movimento AS m
+inner join Produto AS p ON (m.idProduto = p.idProduto)
+inner join Pessoa AS pe ON (m.idPessoa = pe.idPessoa)
+WHERE tipo = 'E'
 
 
+SELECT pdt.nome, SUM(m.quantidade * m.valorUnitario) as valor_total
+FROM Movimento m
+INNER JOIN Produto pdt ON m.idProduto = pdt.idProduto
+WHERE m.tipo = 'S'
+GROUP BY pdt.nome;
 
-SELECT p.*, pf.cpf 
-FROM Pessoa p
-INNER JOIN Pessoa_Fisica pf ON p.idPessoa = pf.idPessoa;
+SELECT pdt.nome, SUM(m.quantidade * m.valorUnitario) as Valor_total
+FROM Movimento m
+INNER JOIN Produto pdt ON m.idProduto = pdt.idProduto
+WHERE m.tipo = 'E'
+GROUP BY pdt.nome;
 
-SELECT p.*, pj.cnpj 
-FROM Pessoa p
-INNER JOIN Pessoa_Juridica pj ON p.idPessoa = pj.idPessoa;
+SELECT u.idUsuario, u.Logar FROM 
+	(SELECT idUsuario FROM Usuario
+	EXCEPT   
+	SELECT idUsuario FROM Movimento AS m
+	WHERE m.tipo = 'E') AS UserSemMovimento
+inner join Usuario AS u ON (u.idUsuario = UserSemMovimento.idUsuario);
+
+SELECT u.*
+FROM Usuario u
+LEFT JOIN Movimento m ON u.idUsuario = m.idUsuario AND m.tipo = 'E'
+WHERE m.idMovimento is null;
+
+SELECT u.Logar, sum(quantidade * valorUnitario) AS Valor_Total 
+FROM Movimento AS m
+inner join Usuario AS u ON (m.idUsuario = u.idUsuario)
+WHERE m.tipo = 'E'
+GROUP BY u.Logar
+
+SELECT u.Logar, sum(quantidade * valorUnitario) AS ValorTotal 
+FROM Movimento AS m
+inner join Usuario AS u ON (m.idUsuario = u.idUsuario)
+WHERE m.tipo = 'S'
+GROUP BY u.Logar
+
+SELECT p.nome,  sum(m.quantidade * valorUnitario)/sum(m.quantidade) AS ValorTotal 
+FROM Movimento AS m
+inner join Produto AS p ON (m.idProduto = p.idProduto)
+WHERE m.tipo = 'S'
+GROUP BY p.nome
 
 SELECT m.*, p.nome as fornecedor, pdt.nome as Produto, m.quantidade, m.valorUnitario, (m.quantidade * m.valorUnitario) as total
 FROM Movimento m
@@ -153,23 +258,4 @@ FROM Movimento m
 INNER JOIN Pessoa p ON m.idPessoa = p.idPessoa
 INNER JOIN Produto pdt ON m.idProduto = pdt.idProduto
 WHERE m.tipo = 'S';
-
-SELECT pdt.nome, SUM(m.quantidade * m.valorUnitario) as totValor
-FROM Movimento m
-INNER JOIN Produto pdt ON m.idProduto = pdt.idProduto
-WHERE m.tipo = 'E'
-GROUP BY pdt.nome;
-
-SELECT pdt.nome, SUM(m.quantidade * m.valorUnitario) as valor_total
-FROM Movimento m
-INNER JOIN Produto pdt ON m.idProduto = pdt.idProduto
-WHERE m.tipo = 'S'
-GROUP BY pdt.nome;
-
-SELECT u.*
-FROM Usuario u
-LEFT JOIN Movimento m ON u.idUsuario = m.idUsuario AND m.tipo = 'E'
-WHERE m.idMovimento is null;
-
-
 
